082bbdb467f9814a05f7da81f37b2bf10af4995b
Paolo Bellardone <paolo.bellardone@outlook.com>
2025-03-12 15:52:34 +0100

    Updated helidon and OracleJet versions

--- src/main/resources/WEB/js/libs/oraclejet-preact/amd/VirtualizedListView-554eaee6.js
+++
@@ -1,2 +0,0 @@
-define(['exports', 'preact/jsx-runtime', './List-04c4fed0'], (function(i,e,t){"use strict";i.VirtualizedListView=function(i){return e.jsx(t.List,{isVirtualized:!0,...i,children:i.children})}}));
-//# sourceMappingURL=VirtualizedListView-554eaee6.js.map
